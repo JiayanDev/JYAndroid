92ee9197ffdffe21943eebdda1da287db3ca30fa
liangzili <lzl0120@gmail.com>
2015-07-02 15:18:42 +0800

    update

---
+++ library/src/main/java/com/jiayantech/library/base/BaseModel.java
@@ -0,0 +1,14 @@
+package com.jiayantech.library.base;
+
+/**
+ * Created by janseon on 2015/6/30.
+ *
+ * @Description:
+ * @Copyright: Copyright (c) 2015 Shenzhen Jiayan Tech Co., Ltd. Inc. All
+ * rights reserved.
+ */
+public class BaseModel {
+    public long id;
+    public long createTime;
+
+}
