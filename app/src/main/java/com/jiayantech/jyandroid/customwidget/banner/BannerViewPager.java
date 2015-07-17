4bcaa917e2df98e07e78cd7ca996dc95598a3dc5
liangzili <lzl0120@gmail.com>
2015-07-17 14:09:52 +0800

    update

--- app/src/main/java/com/jiayantech/jyandroid/customwidget/banner/BannerViewPager.java
+++ app/src/main/java/com/jiayantech/jyandroid/customwidget/banner/BannerViewPager.java
@@ -2,6 +2,7 @@ package com.jiayantech.jyandroid.customwidget.banner;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 
 import java.util.List;
 
@@ -17,13 +18,24 @@ public class BannerViewPager extends AutoScrollViewPager {
     public BannerViewPager(Context paramContext) {
         super(paramContext);
         mContext = paramContext;
-        this.setCycle(false);
+        this.setCycle(true);
     }
 
     public BannerViewPager(Context paramContext, AttributeSet paramAttributeSet) {
         super(paramContext, paramAttributeSet);
         mContext = paramContext;
+        setCycle(true);
     }
 
-    
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        //super.onInterceptTouchEvent(ev);
+        return true;
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent ev) {
+        super.onTouchEvent(ev);
+        return true;
+    }
 }
